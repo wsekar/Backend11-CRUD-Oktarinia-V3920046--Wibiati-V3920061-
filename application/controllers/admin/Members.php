c811686bb623816127bd405f22c9c666b3ce24b5
Wibiati Sekar K <89697985+wsekar@users.noreply.github.com>
2021-12-02 21:44:46 +0700

    Update Members.php

--- application/controllers/admin/Members.php
+++ application/controllers/admin/Members.php
@@ -11,6 +11,7 @@ class Members extends CI_Controller
         $this->load->library('form_validation'); //load library (form_validation)
         // Library form_validation akan digunakan untuk memvalidasi input pada method add() dan edit()
         $this->load->helper('tanggal'); // load helper tanggal
+		    $this->load->library('upload'); // load library upload
     }
 
     public function index()
@@ -21,74 +22,110 @@ class Members extends CI_Controller
 
     public function add() // menampilkan form add dan menyimpan data ke database
     {
-        if ($this->input->method() === 'post') {
+       if ($this->input->method() === 'post') {
+            $nama_member = $this->input->post('nama_member', TRUE);
+            $tanggal_lahir = $this->input->post('tanggal_lahir', TRUE);
+            $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
+            $kewarganegaraan = $this->input->post('kewarganegaraan', TRUE);
+            $email_member = $this->input->post('email_member', TRUE);
+            $password = $this->input->post('password', TRUE);
+            $deskripsi = $this->input->post('deskripsi', TRUE);
+            $unit = $this->input->post('unit', TRUE);
             $config['upload_path']          = './assets/images/'; //tempat penyimpanan
-            $config['allowed_types']        = 'gif|jpg|png'; //tipe yang ingin diinsert
+            $config['allowed_types']        = 'gif|jpg|png|PNG'; //tipe yang ingin diinsert
             $config['max_size']             = 10000; //ukuran file maksimal
             $config['max_width']            = 10000; //lebar maksimal
             $config['max_height']           = 10000; //tinggi maksimal
-
-            $this->load->library('upload', $config);
-
-            if (!$this->upload->do_upload('foto_member')) { //jika gambar dalam form tidak ada
-                $data['error'] = $this->upload->display_errors();
+            $config['file_name'] = $_FILES['foto_member']['name'];
+            $this->upload->initialize($config);
+            if (!empty($_FILES['foto_member']['name'])) {
+                if ($this->upload->do_upload('foto_member')) {
+                    $foto_member = $this->upload->data();
+                    $data = array(
+                        'foto_member' => $foto_member['file_name'],
+                        'nama_member' => $nama_member,
+                        'tanggal_lahir' => $tanggal_lahir,
+                        'jenis_kelamin' => $jenis_kelamin,
+                        'kewarganegaraan' => $kewarganegaraan,
+                        'email_member' => $email_member,
+                        'password' => $password,
+                        'deskripsi' => $deskripsi,
+                        'unit' => json_encode(implode(",", $unit)),
+                    );
+                    $this->db->insert('tb_member', $data);
+                    $this->session->set_flashdata('success', ' Data berhasil disimpan');
+                    redirect(site_url('admin/members'));
+                } else {
+                    die("Foto gagal diupload");
+                }
             } else {
-                $foto_member = $this->upload->data(); // data upload foto
-                $foto_member = $foto_member['file_name']; 
-                $nama_member = $this->input->post('nama_member', TRUE); // inputan data nama_member
-                $tanggal_lahir = $this->input->post('tanggal_lahir', TRUE); // inputan data tanggal_lahir
-                $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE); // inputan data jenis_kelamin
-                $kewarganegaraan = $this->input->post('kewarganegaraan', TRUE); // inputan data kewarganegaraan
-                $email_member = $this->input->post('email_member', TRUE); // inputan data email_member
-                $password = $this->input->post('password', TRUE); // inputan data password
-                $deskripsi = $this->input->post('deskripsi', TRUE); // inputan data dekripsi
-                $unit = $this->input->post('unit', TRUE); // inputan dataunit
-
-                $data = array( // variable data yang akan menampung sekumpulan data hasil inputan tadi
-                    'nama_member' => $nama_member,
-                    'tanggal_lahir' => $tanggal_lahir,
-                    'jenis_kelamin' => $jenis_kelamin,
-                    'kewarganegaraan' => $kewarganegaraan,
-                    'email_member' => $email_member,
-                    'password' => $password,
-                    'deskripsi' => $deskripsi,
-                    'unit' => $unit,
-                );
-                // $member->save(); // simpan data ke database
-                $this->db->insert('tb_member', $data);
-                $this->session->set_flashdata('success', ' Data berhasil disimpan'); //notifikasi data berhasil disimpan
+                echo "data tidak masuk";
             }
-            // menampilkan form add
-            // $member = $this->M_member; // objek model
-            // $validation = $this->form_validation; // objek form validation
-            // $validation->set_rules($member->rules()); // terapkan rules
-            // if ($validation->run()) { // melakukan validasi
-            //     $member->save(); // simpan data ke database
-            //     $this->session->set_flashdata('success', 'Data berhasil disimpan'); // pesan berhasil
-            // }
-            // $this->load->view("admin/member/add_member");
+        } else {
+
+            // $this->session->set_flashdata('success', ' Data tidak berhasil disimpan');
+            $this->load->view("admin/member/add_member");
         }
-        $this->load->view("admin/member/add_member");
     }
 
     // $id -> data yang akan diedit , 
     public function edit($id = null)
     {                    // dilakukan rederict ke route admin/members jika $id bernilai null
-        if (!isset($id)) redirect('admin/members');
-
-        $member = $this->M_member; // objek model
+         if (!isset($id)) redirect('admin/members');
+        $member = $this->M_member;
         $validation = $this->form_validation; // objek validation
-        $validation->set_rules($member->rules()); //menerapkan rules
+        $validation->set_rules($member->rules());
+        $data["member"] = $member->getById($id);
 
-        if ($validation->run()) { // melakukan validasi
-            $member->update(); // menyim[an data yang telah di edit
-            $this->session->set_flashdata('success', ' Data berhasil disimpan');
+        if ($this->input->method() === 'post') {
+            $id_member = $this->input->post('id', TRUE);
+            $nama_member = $this->input->post('nama_member', TRUE);
+            $tanggal_lahir = $this->input->post('tanggal_lahir', TRUE);
+            $jenis_kelamin = $this->input->post('jenis_kelamin', TRUE);
+            $kewarganegaraan = $this->input->post('kewarganegaraan', TRUE);
+            $email_member = $this->input->post('email_member', TRUE);
+            $password = $this->input->post('password', TRUE);
+            $deskripsi = $this->input->post('deskripsi', TRUE);
+            $unit = $this->input->post('unit', TRUE);
+            $path = './assets/image/';
+            $condition = array('id_member' => $id_member);
+            $config['upload_path']          = './assets/images/'; //tempat penyimpanan
+            $config['allowed_types']        = 'gif|jpg|png|PNG'; //tipe yang ingin diinsert
+            $config['max_size']             = 10000; //ukuran file maksimal
+            $config['max_width']            = 10000; //lebar maksimal
+            $config['max_height']           = 10000; //tinggi maksimal
+            $config['file_name'] = $_FILES['foto_member']['name'];
+            $this->upload->initialize($config);
+            if (!empty($_FILES['foto_member']['name'])) {
+                if ($this->upload->do_upload('foto_member')) {
+                    $foto_member = $this->upload->data();
+                    $data = array(
+                        'foto_member' => $foto_member['file_name'],
+                        'nama_member' => $nama_member,
+                        'tanggal_lahir' => $tanggal_lahir,
+                        'jenis_kelamin' => $jenis_kelamin,
+                        'kewarganegaraan' => $kewarganegaraan,
+                        'email_member' => $email_member,
+                        'password' => $password,
+                        'deskripsi' => $deskripsi,
+                        'unit' => json_encode(implode(",", $unit)),
+                    );
+                    @unlink($path . $this->input->post('foto_lama'));
+
+                    $this->db->update('tb_member', $data, $condition);
+                    $this->session->set_flashdata('success', ' Data berhasil disimpan');
+                    redirect(site_url('admin/members'));
+                } else {
+                    die("Foto gagal diupload");
+                }
+            } else {
+                echo "data gagal diupdate";
+            }
+        } else {
+
+            // $this->session->set_flashdata('success', ' Data tidak berhasil disimpan');
+            $this->load->view("admin/member/edit_member", $data);
         }
-
-        $data["member"] = $member->getById($id); // mengambil data untuk ditampilkan pada form edit
-        if (!$data["member"]) show_404(); // apabila data tidak ada maka akan eror 404
-
-        $this->load->view("admin/member/edit_member", $data); // menampilkan form edit
     }
 
     // $id untuk menentukkan data mana yang akan dihapus
